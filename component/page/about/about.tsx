0ed9238ce1d174a24a49e9c42969254e4bd4df46
SatooRu65536 <ryuto4619@gmail.com>
2023-09-12 20:18:00 +0900

    add: about 追加

--- component/page/about/about.tsx
+++ component/page/about/about.tsx
@@ -2,8 +2,20 @@ import styles from "./about.module.scss";
 
 export default function About() {
   return (
-    <div className={styles.about}>
-      <div className={styles.name}>SatooRu</div>
-    </div>
-  )
+    <section className={styles.about}>
+      <div className={styles.container}>
+        <h1 className={styles.name}>SatooRu</h1>
+
+        <div className={styles.profile}>
+          <p>
+            名古屋の大学生。
+            プログラミングが好きで主にフロントエンドをしています。
+            夢はペンギンと同居すること。
+            お世話は面倒なので、いずれペンギン型ロボットが誕生することを願っています。
+          </p>
+          <p>ペンギン・城・歴史がめっちゃすきです。</p>
+        </div>
+      </div>
+    </section>
+  );
 }
