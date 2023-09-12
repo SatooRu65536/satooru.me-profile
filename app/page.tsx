04cef9447eca3890a515a6389d4cf026afab5719
SatooRu65536 <ryuto4619@gmail.com>
2023-09-12 23:12:59 +0900

    rename: page→section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-import Top from "@/component/page/top/top";
 import styles from "./page.module.scss";
-import About from "@/component/page/about/about";
+import Top from "@/component/section/top/top";
+import About from "@/component/section/about/about";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Top />
-      <About />
       <About />
     </main>
   );
