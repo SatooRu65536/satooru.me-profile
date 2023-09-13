0b4a74bb4463c3190bc61f12ac01a9bd366ea820
SatooRu65536 <ryuto4619@gmail.com>
2023-09-13 21:23:04 +0900

    add: 404ページ追加

---
+++ app/not-found.tsx
@@ -0,0 +1,5 @@
+import NotFound from "@/component/base/notfound/notfound";
+
+export default function NotFoundPage() {
+  return <NotFound />;
+}
---
+++ component/base/notfound/notfound.tsx
@@ -0,0 +1,6 @@
+"use client";
+import styles from "./notfound.module.scss";
+
+export default function NotFound() {
+  return <div className={styles.notfound}>404</div>;
+}
