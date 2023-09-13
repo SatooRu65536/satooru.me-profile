ee7315dd40755fd328a44ca4108c9d855a61054e
SatooRu65536 <ryuto4619@gmail.com>
2023-09-14 02:47:07 +0900

    delete: ブログ機能を削除

--- component/ui/sidebar/sidebar.tsx
+++
@@ -1,8 +0,0 @@
-import { getPostSlugs } from "@/component/util/util";
-import Tree from "@/component/ui/tree/tree";
-
-export default function Sidebar() {
-  const postSlugs = getPostSlugs();
-
-  return <Tree postSlugs={postSlugs} />;
-}
