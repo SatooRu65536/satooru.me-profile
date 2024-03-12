82b87a0ff6cc191ae6d14cf1becf858997dde91c
satooru65536 <ryuto4619@gmail.com>
2024-03-12 12:22:38 +0900

    update: works更新

--- app/(use-header)/_compoent/section/works/index.tsx
+++ app/(use-header)/_compoent/section/works/index.tsx
@@ -4,10 +4,19 @@ import { Work as WordT } from '@/types/';
 export default function Works() {
   const works: WordT[] = [
     {
+      genre: 'コメダ珈琲',
+      name: 'バイト',
+      start: '2023.02',
+      end: '2023.11',
+      summary: 'ホールスタッフ',
+      technologies: [],
+    },
+    {
       genre: '個人',
       name: '業務委託',
       summary: 'ウェブメディアを管理するwebシステム開発',
       start: '2023.09',
+      end: '2024.02',
       technologies: [
         'Nest.js',
         'vue.js',
@@ -23,7 +32,15 @@ export default function Works() {
       name: '業務委託',
       summary: '印刷会社のwebシステム開発',
       start: '2023.09',
+      end: '2024.02',
       technologies: ['Rails', 'Haml', 'Bootstrap', 'jQuery', 'DataTables'],
+    },
+    {
+      genre: 'pluszero',
+      name: 'インターン(バイト)',
+      start: '2024.02',
+      summary: 'Webシステム開発',
+      technologies: ['React.js', 'Nest.js'],
     },
   ];
 
